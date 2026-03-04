8a3bb9965c96d163f3e729b0d77eae6ac4c7398b
rdesfonds-campus <romain.desfonds@gmail.com>
2026-03-04 16:04:47 +0100

    Créer une case vide (EmptyCell) #3

---
+++ src/fr/campus/wonderboys/game/EnemyCell.java
@@ -0,0 +1,17 @@
+package fr.campus.wonderboys.game;
+
+/**
+ * Case contenant un ennemi sur le plateau.
+ *
+ * Le joueur devra combattre l'ennemi s'il tombe dessus.
+ *
+ * @author Romain D
+ * @version 1.0
+ */
+public class EnemyCell extends Cell {
+
+    @Override
+    public String toString() {
+        return "Case ennemi";
+    }
+}
---
+++ src/fr/campus/wonderboys/game/WeaponCell.java
@@ -0,0 +1,17 @@
+package fr.campus.wonderboys.game;
+
+/**
+ * Case contenant une arme à ramasser.
+ *
+ * Le joueur peut s'équiper de l'arme pour augmenter sa force.
+ *
+ * @author Romain D
+ * @version 1.0
+ */
+public class WeaponCell extends Cell {
+
+    @Override
+    public String toString() {
+        return "Case arme";
+    }
+}
