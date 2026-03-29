975aa98a9545ea8432df3a2258578e4817497122
rdesfonds-campus <romain.desfonds@le-campus-numerique.fr>
2026-03-30 00:22:51 +0200

    Bouger le joueur sur le plateau

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,6 +1,5 @@
 package game;
 
-import characters.Character;
 import characters.Warrior;
 import characters.Wizard;
 import equipment.OffensiveEquipment;
@@ -8,37 +7,46 @@ import equipment.DefensiveEquipment;
 
 public class Game {
 
-        private Menu menu;
-        private characters.Character currentCharacter;
+    private Menu menu;
+    private characters.Character currentCharacter;
+    private int position;
 
-        public Game(Menu menu) {
-            this.menu = menu;
-        }
+    public Game(Menu menu) {
+        this.menu = menu;
+        this.position = 1;
+    }
 
-        public void start() {
-            menu.showMessage("Bienvenue dans Wonderboys !");
-            menu.showMessage("===========================");
+    public void start() {
+        menu.showMessage("Bienvenue dans Wonderboys !");
+        menu.showMessage("=========================");
 
-            int choix = 0;
-            while (choix != 2) {
-                menu.showMessage("1 - Créer un personnage");
-                menu.showMessage("2 - Quitter");
-                choix = menu.askInt("Ton choix :");
+        int choix = 0;
+        while (choix != 3) {
+            menu.showMessage("1 - Créer un personnage");
+            menu.showMessage("2 - Jouer");
+            menu.showMessage("3 - Quitter");
+            choix = menu.askInt("Ton choix :");
 
-                switch (choix) {
-                    case 1:
-                        creerPersonnage();
-                        break;
-                    case 2:
-                        menu.showMessage("Au revoir !");
-                        break;
-                    default:
-                        menu.showMessage("Choix invalide.");
-                        break;
-                }
+            switch (choix) {
+                case 1:
+                    creerPersonnage();
+                    break;
+                case 2:
+                    if (currentCharacter == null) {
+                        menu.showMessage("Crée d'abord un personnage !");
+                    } else {
+                        jouer();
+                    }
+                    break;
+                case 3:
+                    menu.showMessage("Au revoir !");
+                    break;
+                default:
+                    menu.showMessage("Choix invalide.");
+                    break;
             }
-
-}
+        }
+    }
 
     private void creerPersonnage() {
         menu.showMessage("Quel type de personnage veux-tu ?");
@@ -46,7 +54,6 @@ public class Game {
         menu.showMessage("2 - Wizard");
 
         int type = menu.askInt("Ton choix :");
-
         String nom = menu.askString("Quel est le nom de ton personnage ?");
 
         if (type == 1) {
@@ -61,5 +68,36 @@ public class Game {
 
         menu.showMessage("Personnage créé !");
         menu.showMessage(currentCharacter.toString());
+    }
+
+    private void jouer() {
+        Board plateau = new Board();
+        Dice de = new Dice(6);
+        position = 1;
+
+        menu.showMessage("\n--- Début de la partie ! ---");
+        menu.showMessage(currentCharacter.getName() + " entre dans le donjon.");
+
+        while (position < plateau.getTotalCases()) {
+            menu.askString("Appuie sur Entrée pour lancer le dé...");
+
+            int lancer = de.roll();
+            int nouvellePosition = position + lancer;
+
+            menu.showMessage("Tu lances le dé : " + lancer);
+
+            if (nouvellePosition >= plateau.getTotalCases()) {
+                menu.showMessage("Tu arrives à la case 64 - Salle du trésor !");
+                menu.showMessage("VICTOIRE ! Bien joué " + currentCharacter.getName() + " !");
+                break;
+            }
+
+            position = nouvellePosition;
+            menu.showMessage("Tu avances à la case " + position + " / " + plateau.getTotalCases());
+
+            Cell caseActuelle = plateau.getCase(position);
+            menu.showMessage("Tu es sur : " + caseActuelle);
+            caseActuelle.interact(currentCharacter);
+        }
     }
 }
